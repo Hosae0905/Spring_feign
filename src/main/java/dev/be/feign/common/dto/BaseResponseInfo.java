e7218b4f9a2fc5301883d6d082ae91e3d3a475b0
didgh <didghtls11@gmail.com>
2023-09-04 19:12:52 +0900

    Feign 공부

---
+++ src/main/java/dev/be/feign/common/dto/BaseResponseInfo.java
@@ -0,0 +1,20 @@
+package dev.be.feign.common.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.*;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(Include.NON_NULL)
+public class BaseResponseInfo {
+    private String name;
+    private String header;
+    private Long age;
+}
